20a8071dede1f2ed66e63be91651e5f63909698b
d1ana96 <diana.petkova96@gmail.com>
2023-01-15 12:55:27 +0200

    new file added

---
+++ Programming Basics - Python/conditional_statements_advanced_lab/animal_type.py
@@ -0,0 +1,7 @@
+type_animal = input()
+if type_animal == "dog":
+    print("mammal")
+elif type_animal == "crocodile" or type_animal == "tortoise" or type_animal == "snake":
+    print("reptile")
+else:
+    print("unknown")
