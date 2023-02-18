b2a69810416c0a2f6df2efd8da5a6ea05c437636
d1ana96 <diana.petkova96@gmail.com>
2023-02-18 19:37:14 +0200

    new file added

---
+++ Programin Fundamentals - Python/basic_syntax_lab/even_numbers.py
@@ -0,0 +1,9 @@
+count_numbers = int(input())
+
+for i in range(count_numbers):
+    number = int(input())
+    if not number % 2 == 0:
+        print(f"{number} is odd!")
+        break
+else:
+    print("All numbers are even.")
