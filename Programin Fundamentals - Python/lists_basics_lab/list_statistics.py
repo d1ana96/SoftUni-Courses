a04a88e626ac633b56c42f3d603fc9a5df6b3d60
d1ana96 <diana.petkova96@gmail.com>
2023-03-07 23:36:25 +0200

    new file added

---
+++ Programin Fundamentals - Python/lists_basics_lab/list_statistics.py
@@ -0,0 +1,17 @@
+number = int(input())
+positives = list()
+negatives = list()
+
+sum_of_negatives = 0
+
+for i in range(number):
+    current_number = int(input())
+    if current_number >= 0:
+        positives.append(current_number)
+    else:
+        negatives.append(current_number)
+        sum_of_negatives += current_number
+print(positives)
+print(negatives)
+print(f"Count of positives: {len(positives)}")
+print(f"Sum of negatives: {sum_of_negatives}")
